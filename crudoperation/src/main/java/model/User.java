648a2e7109e7c860df035a63c62fb407efbeee6d
Niphilbert <125033597+Niphilbert@users.noreply.github.com>
2024-02-29 10:12:25 +0200

    Add files via upload

---
+++ crudoperation/src/main/java/model/User.java
@@ -0,0 +1,70 @@
+package model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users") 
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name, password, email, sex, country;
+
+    // Getter and Setter for id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter and Setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter and Setter for password
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // Getter and Setter for email
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Getter and Setter for sex
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    // Getter and Setter for country
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
+
+
+    
+
---
+++ crudoperation/target/crudoperation/index.jsp
@@ -0,0 +1,26 @@
+<!DOCTYPE html>  
+<html>  
+<head>  
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">  
+<title>JSP CRUD Example</title>  
+</head>  
+<body>  
+<h1>JSP CRUD Example</h1>  
+<a href="adduserform.jsp">Add User</a>  
+  
+  
+</body>  
+</html>  
+
+<!DOCTYPE html>  
+<html>  
+<head>  
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">  
+<title>Add User Form</title>  
+</head>  
+<body>  
+  
+<jsp:include page="userform.html"></jsp:include>  
+  
+</body>  
+</html>  
